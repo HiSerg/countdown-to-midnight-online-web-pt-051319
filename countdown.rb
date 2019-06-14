0923c13f0c07530dc97d2f76637ac70d275844c1
Sergio Guzman <hiserg98@gmail.com>
2019-06-14 04:26:44 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -11,11 +11,11 @@ end
 
 countdown(10)
 
-def countdown_with_sleep(number)
+def countdown_with_sleep(numbers)
   while numbers > 0
    sleep 1
-    puts "#{number} SECOND(S)!"
-    number -= 1
+    puts "#{numbers} SECOND(S)!"
+    numbers -= 1
   end
   return "HAPPY NEW YEAR!"
 end
