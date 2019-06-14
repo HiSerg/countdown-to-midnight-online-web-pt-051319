57126bf61666fd921108311a50e5364b2483c6ec
Sergio Guzman <hiserg98@gmail.com>
2019-06-14 04:25:11 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,13 @@
 #write your code here
 
-def countdown
+def countdown (numbers)
+
+counter = 10 - numbers
+while counter < 10 && numbers < 10
+sleep 1
+puts "#{numbers} SECOND(S)!"
+counter += 1
+numbers -= 1
+end
+puts "HAPPY NEW YEAR!"
 end
